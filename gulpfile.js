e5f40756ba65d21aefdb514c5cd6d8ab72e98047
Josh <joshhazn@gmail.com>
2017-07-06 22:22:40 -0400

    Add new site assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,76 @@
-//Bring in some dependencies
-var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
-var sourcemaps = require('gulp-sourcemaps');
-var sass = require('gulp-sass');
+'use strict';
 
+var gulp         = require('gulp'),
+    gutil        = require('gulp-util'),
+    plumber      = require('gulp-plumber'),
+    sass         = require('gulp-sass'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleancss     = require('gulp-clean-css'),
+    jshint       = require('gulp-jshint'),
+    stylish      = require('jshint-stylish'),
+    uglify       = require('gulp-uglify'),
+    concat       = require('gulp-concat'),
+    rename       = require('gulp-rename'),
+    browser      = require('browser-sync').create();
 
-var paths = {
-  scripts: './assets/js/*',
-  images: './assets/images/**/*',
-  styles: './assets/scss/*.scss'
-};
-
- 
-gulp.task('scripts', function() {
-  return gulp.src([
-    './assets/js/main.js'
-    ])
-  .pipe(sourcemaps.init())
-  .pipe(uglify())
-  .pipe(concat('all.js'))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('build/js'));
+gulp.task('browser-sync', ['styles'], function () {
+    browser.init({
+        server:{
+            baseDir: "./"
+        }
+    });
 });
 
-
-gulp.task('images', function() {
-  return gulp.src(paths.images)
-    .pipe(imagemin({optimizationLevel: 3}))
-    .pipe(gulp.dest('build/img'));
+gulp.task('styles', function () {
+    return gulp.src('./assets/scss/app.scss')
+        .pipe(plumber(function (error) {
+            gutil.log(gutil.colors.red(error.message));
+            this.emit('end');
+        }))
+        .pipe(sass({ includePaths : ['node_modules/foundation-sites/scss']}))
+        .pipe(autoprefixer({
+            browsers: ['> 1%', 'last 5 versions', 'ie >= 9', 'Firefox ESR'],
+            cascade : false
+        }))
+        .pipe(gulp.dest('./css/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cleancss({compatibility: 'ie9'}))
+        .pipe(gulp.dest('./css/'))
+        .pipe(browser.reload({stream:true}));
 });
 
+gulp.task('fonts', function () {
+    var files = [
+        './assets/fonts/*.*'
+    ];
 
-// Convert Sass => css
-gulp.task('sass', function() {
-  return gulp.src(paths.styles)
-    // Pass in options to the task 
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('build/css'));
+    return gulp.src(files)
+        .pipe(gulp.dest('./fonts/dist'));
 });
 
+// gulp.task('scripts', function () {
+//     var files = ['./assets/js/*.js'];
 
-gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.images, ['images']);
-  gulp.watch(paths.styles, ['sass']);
+//     return gulp.src(files)
+//         .pipe(plumber())
+//         .pipe(jshint())
+//         .pipe(jshint.reporter('jshint-stylish'))
+//         .pipe(concat('scripts.js'))
+//         .pipe(gulp.dest('./js/dist'))
+//         .pipe(rename({suffix: '.min'}))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('./js/dist'))
+//         .pipe(browser.reload({stream:true}));
+
+// });
+
+gulp.task('default', function () {
+    gulp.start('styles', 'fonts');
 });
- 
 
-gulp.task('default', ['watch', 'scripts', 'images','sass']);
-
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch('./assets/scss/*.scss', ['styles']);
+    // gulp.watch('./assets/js/*.js', ['scripts']);
+    gulp.watch("*.html").on('change', browser.reload);
+});
