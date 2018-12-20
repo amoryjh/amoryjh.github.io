5c507079385c4e4448560aa346fa8d59f5f65fc7
Josh Hanson <joshhazn@gmail.com>
2018-12-19 22:26:09 -0500

    Gatsby build

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('/Users/joshhanson/Documents/myproject/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
       plugin: require('/Users/joshhanson/Documents/myproject/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,6 +3,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/joshhanson/Documents/myproject/node_modules/gatsby-plugin-offline/app-shell.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/joshhanson/Documents/myproject/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/joshhanson/Documents/myproject/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/Users/joshhanson/Documents/myproject/src/pages/page-2.js"))
