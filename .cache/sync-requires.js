bbdd7f15d8c8c4613e1e8329da3d450e273b49d4
Josh Hanson <joshhazn@gmail.com>
2021-06-07 00:51:55 -0400

    Update gatsby site

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/joshhanson/Documents/myproject/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/joshhanson/Documents/myproject/node_modules/gatsby-plugin-offline/app-shell.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/joshhanson/Documents/myproject/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/joshhanson/Documents/myproject/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/joshhanson/Documents/myproject/src/pages/page-2.js"))),
